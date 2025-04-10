8852cea994daf823f6c11c51824a07df8a1c4218
Archil-Milorava <achimilorava16@gmail.com>
2025-04-10 15:54:38 +0400

    cors policy changes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-import express from "express";
-import "dotenv/config";
-import userRoutes from "./routes/user/user.routes.js";
-import errorHandler from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import errorHandler from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/user/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -18,10 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "https://your-app-name.netlify.app"],
     credentials: true,
   })
 );
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to my backend" });
 });
