159415f7b2a2f322b2ef4818bd4e937ed47fea41
Archil-Milorava <achimilorava16@gmail.com>
2025-04-12 23:17:28 +0400

    fixing cookie

--- src/utils/accessToken.js
+++ src/utils/accessToken.js
@@ -9,7 +9,7 @@ export const handleAccessToken = (userId, res) => {
     maxAge: 360 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
   });
 
   return accessToken;
